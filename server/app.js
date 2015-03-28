1faee1d2a0daefa5b7212052db4d69c16e461fdb
Gustave (Gus) Arthur Monod <gustave.monod@insa-lyon.fr>
2015-03-29 00:50:04 +0100

    Added basic play routes

--- server/app.js
+++ server/app.js
@@ -4,6 +4,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var play = require('play').Play();
+
 var app = express();
 
 app.use(logger('dev'));
@@ -12,26 +14,31 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+var playlistPaths = {
+	'a76f8c': '/home/pi/musics/Epic Sax Guy _.mp3',
+	'b5da8': '/home/pi/musics/match0.wav'
+};
+
 var examplePlaylist = {
   playlist: {
     current : {
-      id: "a76f8c",
-      name: "Super Mario Bros. Theme",
-      img: "https://upload.wikimedia.org/wikipedia/en/9/99/MarioSMBW.png",
-      artist: "Gregg Pollack"
+      id: 'a76f8c',
+      name: 'Super Mario Bros. Theme',
+      img: 'https://upload.wikimedia.org/wikipedia/en/9/99/MarioSMBW.png',
+      artist: 'Gregg Pollack'
     },
     following: [
       {
-        id: "b5da8",
-        name: "Doctor Who Theme",
+        id: 'b5da8',
+        name: 'Doctor Who Theme',
         img: null,
         artist: null,
         likes: 13,
         dyslikes: 2
       },
       {
-        id: "b5da8",
-        name: "Doctor Who Theme",
+        id: 'b5da8',
+        name: 'Doctor Who Theme',
         img: null,
         artist: null,
         likes: 13,
@@ -41,8 +48,18 @@ var examplePlaylist = {
   }
 };
 
-app.use('/playlist', function(req, res, next) {
+app.get('/playlist', function(req, res, next) {
 	res.send(JSON.stringify(examplePlaylist));
+});
+
+app.get('/playcurrent', function(req, res, next) {
+	play.sound(playlistPaths[examplePlaylist.playlist.current.id]);
+	res.send('ok');
+});
+
+app.get('/play/:id', function(req, res, next) {
+//	play.sound(playlistPaths[req.id]);
+	res.send('ok');
 });
 
 // catch 404 and forward to error handler
